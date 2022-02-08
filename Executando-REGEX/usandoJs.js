4e844f830b399307a6e4ba35c6bcaedc1f3748b5
Maxel-Uds <maxellopes32@gmail.com>
2022-02-08 14:00:33 -0300

    Executando Regex com JavaScript

---
+++ Executando-REGEX/usandoJs.js
@@ -0,0 +1,16 @@
+const texto = '0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f';
+
+//Função para criar um expressão regular
+const regexNove = RegExp('9');
+
+console.log('Métodos da RegExp...');
+console.log(regexNove.test(texto));
+console.log(regexNove.exec(texto));
+
+const regexLetras = /[a-f]/g;
+
+console.log('Métodos da String...');
+console.log(texto.match(regexLetras));
+console.log(texto.search(regexLetras));
+console.log(texto.replace(regexLetras, '**'));
+console.log(texto.split(regexLetras));
