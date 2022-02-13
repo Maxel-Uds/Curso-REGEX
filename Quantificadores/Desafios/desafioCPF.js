9d6a34a697da0fdf6e6e09be8cf1093beee9be29
Maxel-Uds <maxellopes32@gmail.com>
2022-02-13 13:20:31 -0300

    Desafio do CPF (minha resolução)

---
+++ Quantificadores/Desafios/desafioCPF.js
@@ -0,0 +1,9 @@
+const texto = `CPF dos aprovados: 
+- 600.567.890-12
+- 765.998.345-23
+- 234.634.908-45
+- 456.342.889-56
+- 675.789.203-09`;
+
+const regex = /\d.+\d/g;
+console.log(texto.match(regex));
