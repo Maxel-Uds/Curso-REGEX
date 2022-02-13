3ec713f85c9b250e9e3b42d9cf5e683e65ecd7a4
Maxel-Uds <maxellopes32@gmail.com>
2022-02-13 13:57:59 -0300

    Desafio da listade emails (resoluções do professor)

--- Quantificadores/Desafios/desafioEmail.js
+++ Quantificadores/Desafios/desafioEmail.js
@@ -1,7 +1,15 @@
 const texto = `Emails dos convidados:
 - fulano@cod3r.com.br
 - chico@gmail.com
-- maxellopes32@gmail.com`;
+- maxellopes32@gmail.com
+- joao@empresa.info.br
+- maria_silva@registro.br
+- rafa.sampaio@yahoo.com`;
 
 const regex = /\w.+@.+/g;
 console.log(texto.match(regex));
+
+console.log(texto.match(/[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.[a-zA-Z0-9_]{2,4}/g));
+console.log(texto.match(/\w+@\w+\.\w{2,4}/g));
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}/g));
+console.log(texto.match(/[\w.]+@\w+\.\w{2,4}\.?\w{0,2}/g));
